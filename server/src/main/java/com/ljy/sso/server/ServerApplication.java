ac6c30f72f8a9b3e1c65e20dfa631499513f690e
lijianyuan <Ljy@123456>
2021-06-02 17:55:48 +0800

    sso 认证中心 启动类

--- server/src/main/java/com/ljy/sso/server/ServerApplication.java
+++ server/src/main/java/com/ljy/sso/server/ServerApplication.java
@@ -11,12 +11,6 @@ public class ServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
-        String  tokenId = SsoSessionIdHelper.makeTokenId("15810874514","123456");
-        SsoUser u = new  SsoUser();
-        u.setName("name");
-        SsoTokenLoginHelper.login(tokenId,u);
-        SsoUser uu = (SsoUser)SsoTokenLoginHelper.getUser(tokenId);
-        System.out.println(uu.getName());
     }
 
 }
