99a21f8bb5db85a2ba9d6c7ca441ccc552c2b67f
lijianyuan <Ljy@123456>
2021-06-02 16:22:14 +0800

    sso core user 类

--- core/src/main/java/entity/SsoUser.java
+++ core/src/main/java/entity/SsoUser.java
@@ -2,6 +2,7 @@ package entity;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
 /**
  * @Description: 用户中心 用户类
@@ -12,28 +13,37 @@ import java.util.Date;
 
 public class SsoUser implements Serializable {
     private static final long serialVersionUID = 123456789L;
+
+    private String password;
+    private String mobile;
+
     private Integer id;
     private String name;
     private String nickname;
-    private String mobile;
-    private Integer gender;
-    private String qq;
-    private String email;
-    private String password;
-    private String headUrl;
-    private Integer creatorId;
-    private Date createTime;
-    private Integer modifierId;
-    private Date modifyTime;
-    private Integer del;
-    private Integer source;
-    private Integer activeStatus;
     private  String  userInfo;
-    private  int status;
     private String unique ;
     private int expireMinute;
     private long expireFreshTime;
 
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
 
     public Integer getId() {
         return id;
@@ -59,124 +69,12 @@ public class SsoUser implements Serializable {
         this.nickname = nickname;
     }
 
-    public String getMobile() {
-        return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    public Integer getGender() {
-        return gender;
-    }
-
-    public void setGender(Integer gender) {
-        this.gender = gender;
-    }
-
-    public String getQq() {
-        return qq;
-    }
-
-    public void setQq(String qq) {
-        this.qq = qq;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getHeadUrl() {
-        return headUrl;
-    }
-
-    public void setHeadUrl(String headUrl) {
-        this.headUrl = headUrl;
-    }
-
-    public Integer getCreatorId() {
-        return creatorId;
-    }
-
-    public void setCreatorId(Integer creatorId) {
-        this.creatorId = creatorId;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Integer getModifierId() {
-        return modifierId;
-    }
-
-    public void setModifierId(Integer modifierId) {
-        this.modifierId = modifierId;
-    }
-
-    public Date getModifyTime() {
-        return modifyTime;
-    }
-
-    public void setModifyTime(Date modifyTime) {
-        this.modifyTime = modifyTime;
-    }
-
-    public Integer getDel() {
-        return del;
-    }
-
-    public void setDel(Integer del) {
-        this.del = del;
-    }
-
-    public Integer getSource() {
-        return source;
-    }
-
-    public void setSource(Integer source) {
-        this.source = source;
-    }
-
-    public Integer getActiveStatus() {
-        return activeStatus;
-    }
-
-    public void setActiveStatus(Integer activeStatus) {
-        this.activeStatus = activeStatus;
-    }
-
     public String getUserInfo() {
         return userInfo;
     }
 
     public void setUserInfo(String userInfo) {
         this.userInfo = userInfo;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
     }
 
     public String getUnique() {
@@ -201,6 +99,27 @@ public class SsoUser implements Serializable {
 
     public void setExpireFreshTime(long expireFreshTime) {
         this.expireFreshTime = expireFreshTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SsoUser ssoUser = (SsoUser) o;
+        return expireMinute == ssoUser.expireMinute &&
+                expireFreshTime == ssoUser.expireFreshTime &&
+                Objects.equals(password, ssoUser.password) &&
+                Objects.equals(mobile, ssoUser.mobile) &&
+                Objects.equals(id, ssoUser.id) &&
+                Objects.equals(name, ssoUser.name) &&
+                Objects.equals(nickname, ssoUser.nickname) &&
+                Objects.equals(userInfo, ssoUser.userInfo) &&
+                Objects.equals(unique, ssoUser.unique);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(password, mobile, id, name, nickname, userInfo, unique, expireMinute, expireFreshTime);
     }
 }
 
