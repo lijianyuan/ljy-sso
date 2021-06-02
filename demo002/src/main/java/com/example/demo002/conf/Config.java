396c08cd6b6430e9ef064d3b564d5150b3f79958
lijianyuan <Ljy@123456>
2021-06-02 17:52:39 +0800

    demo2 测试

---
+++ demo002/src/main/java/com/example/demo002/conf/Config.java
@@ -0,0 +1,45 @@
+package com.example.demo002.conf;
+
+import config.SsoConfig;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description: 配置文件
+ * @author: ljy
+ * @date: 2021年06月01日 18:10
+ * @email 15810874514@163.com
+ */
+@Configuration
+public class Config extends SsoConfig {
+
+    /*
+     *@Description: SsoConfig.destroy 注销方法
+     *@Param: 
+     *@return: 
+     *@Author: ljy
+     *@Date: 2021/6/2 9:34
+     *@email: 15810874514@163.com
+     *
+     **/
+    @Override
+    public void destroy() throws Exception {
+        super.destroy();
+    }
+
+    /*
+     *@Description: SsoConfig.afterPropertiesSet  加载Properties参数后设置的
+     *@Param:
+     *@return:
+     *@Author: ljy
+     *@Date: 2021/6/2 9:22
+     *@email: 15810874514@163.com
+     *
+     **/
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        super.afterPropertiesSet();
+    }
+}
+
+    
