56eb030796efe6c12db30b162248f08eac699725
lijianyuan <Ljy@123456>
2021-06-02 17:56:32 +0800

    sso 认证中心 登录服务 需自行实现

--- server/src/main/java/com/ljy/sso/server/service/impl/LoginServiceImpl.java
+++ server/src/main/java/com/ljy/sso/server/service/impl/LoginServiceImpl.java
@@ -10,6 +10,7 @@ import store.SsoSessionIdHelper;
 import store.SsoTokenLoginHelper;
 import util.IPUtil;
 import util.JWTSSOUtil;
+import util.MD5SSOUtil;
 import util.ReturnT;
 
 import java.beans.Transient;
@@ -35,7 +36,7 @@ public class LoginServiceImpl implements LoginService {
     @Override
     @Transient
     public ReturnT login(UserDto userDto) {
-        // 自己公司的业务逻辑 登录验证
+        // 自己公司的业务逻辑 登录验证 MD5SSOUtil 加密用户手机号密码
         return ssoLogin(userDto);
     }
 
@@ -43,32 +44,43 @@ public class LoginServiceImpl implements LoginService {
     @Transient
     public ReturnT ssoLogin(UserDto userDto) {
         // 1、make sso user
-        SsoUser u = new SsoUser();
-        u.setName(userDto.getName());
-        u.setMobile(userDto.getMobile());
-        u.setPassword(userDto.getPassword());
-        u.setUserInfo(JSONObject.toJSONString(userDto));
-        u.setExpireMinute(SsoLoginStore.getRedisExpireMinute());
-        u.setExpireFreshTime(System.currentTimeMillis());
-        u.setUnique(userDto.getUnique());
-        String sessionId = SsoSessionIdHelper.makeTokenId(u.getMobile(),u.getPassword());
-        SsoTokenLoginHelper.login(sessionId, u);
-        return new ReturnT<String>(sessionId);
+
+        try {
+            SsoUser u = new SsoUser();
+            u.setName(userDto.getName());
+            u.setMobile(userDto.getMobile());
+            u.setPassword(userDto.getPassword());
+            u.setUserInfo(JSONObject.toJSONString(userDto));
+            u.setExpireMinute(SsoLoginStore.getRedisExpireMinute());
+            u.setExpireFreshTime(System.currentTimeMillis());
+            u.setUnique(userDto.getUnique());
+            String sessionId = SsoSessionIdHelper.makeTokenId(u.getMobile(),u.getPassword());
+            SsoTokenLoginHelper.login(sessionId, u);
+            return new ReturnT<String>(sessionId);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ReturnT.FAIL;
+        }
     }
 
     @Override
     @Transient
     public ReturnT forgetPwdSubmit(UserDto userDto) {
-        // 自己公司的业务逻辑
+        // 自己公司的业务逻辑 MD5SSOUtil 加密用户手机号密码
 
         // userDto 为最新的用户信息
-        return ssoLogin(userDto);
+        try {
+            return ssoLogin(userDto);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ReturnT.FAIL;
+        }
     }
 
     @Override
     @Transient
     public ReturnT resetPwdSubmit(UserDto userDto) {
-        // 自己公司的业务逻辑
+        // 自己公司的业务逻辑 MD5SSOUtil 加密用户手机号密码
 
         // userDto 为最新的用户信息
         return ssoLogin(userDto);
@@ -83,17 +95,22 @@ public class LoginServiceImpl implements LoginService {
 
     @Override
     public ReturnT loginCheck(String sessionId, String unique) {
-        //
-        // 获取 token 中的 username
-        String mobile = JWTSSOUtil.getUsername(sessionId); // 先获取出手机号
-        // 根据手机好获取用户信息
-        UserDto uuu = new UserDto(); //;userMapper.selectByMobile(AesUtil.encrypt(mobile));
-        uuu.setPassword("123456"); // 假设123456 真实环境请查询数据库
-        SsoUser ssoUser = SsoTokenLoginHelper.loginCheck(sessionId,unique,uuu.getPassword());
-        if (ssoUser == null) {
-            return new ReturnT<SsoUser>(ReturnT.FAIL_CODE, "sso not login.");
+        try {
+            //
+            // 获取 token 中的 username
+            String mobile = JWTSSOUtil.getUsername(sessionId); // 先获取出手机号
+            // 根据手机好获取用户信息
+            UserDto uuu = new UserDto(); //;userMapper.selectByMobile(AesUtil.encrypt(mobile));
+            uuu.setPassword("123456"); // 假设123456 真实环境请查询数据库
+            SsoUser ssoUser = SsoTokenLoginHelper.loginCheck(sessionId,unique,uuu.getPassword());
+            if (ssoUser == null) {
+                return new ReturnT<SsoUser>(ReturnT.FAIL_CODE, "sso not login.");
+            }
+            return new ReturnT<SsoUser>(ssoUser);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ReturnT<SsoUser>(ReturnT.FAIL_CODE, e.getMessage());
         }
-        return new ReturnT<SsoUser>(ssoUser);
 
     }
 
